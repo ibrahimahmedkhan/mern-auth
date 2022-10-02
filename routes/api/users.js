ca522be501d5f77c53845fc061c29cf5c3201828
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-10-02 18:56:53 +0500

    Completed APIS, with minimal files

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+// const validateRegisterInput = require("../../validation/register");
+// const validateLoginInput = require("../../validation/login");
 // Load User model
 const User = require("../../models/User");
 
@@ -14,11 +14,11 @@ const User = require("../../models/User");
 // @access Public
 router.post("/register", (req, res) => {
   // Form validation
-  const { errors, isValid } = validateRegisterInput(req.body);
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+  // const { errors, isValid } = validateRegisterInput(req.body);
+  // // Check validation
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
@@ -28,7 +28,6 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         password: req.body.password,
       });
-      // Hash password before saving in database
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
@@ -39,6 +38,86 @@ router.post("/register", (req, res) => {
             .catch((err) => console.log(err));
         });
       });
+    }
+  });
+});
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  // Form validation
+  // const { errors, isValid } = validateLoginInput(req.body);
+  // // Check validation
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+  const email = req.body.email;
+  const password = req.body.password;
+  // Find user by email
+  User.findOne({ email }).then((user) => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name,
+        };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926, // 1 year in seconds
+          },
+          (err, token) => {
+            console.log("user logged in");
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+router.get("/", async (req, res) => {
+  User.findOne({ email: req.query.email }).then((user) => {
+    if (user) {
+      bcrypt.compare(req.query.password, user.password).then((isMatch) => {
+        if (isMatch) {
+          const header = req.headers.authorization;
+          if (header) {
+            token = header.split(" ")[1];
+            jwt.verify(token, keys.secretOrKey, (err, userName) => {
+              if (err) {
+                res.status(403).json("invalid token");
+              } else {
+                if (req.query.email === user.email) {
+                  res.json(user);
+                } else {
+                  res.status(401).json("Invalid user");
+                }
+              }
+            });
+          } else {
+            return res.status(401).json("No authorization");
+          }
+        } else {
+          return res.status(401).json("Wrong passwrod");
+        }
+      });
+    } else {
+      return res.status(401).json("Email does not exist");
     }
   });
 });
--- server.js
+++ server.js
@@ -20,5 +20,8 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5001; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
